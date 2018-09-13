bf9a4b60921ceb387c565c3dd35dfe384a7a4a0e
Brad Kent <33756142+Brad-Kent@users.noreply.github.com>
2018-09-13 12:36:16 +1000

    Class Structure imp

--- a2.py
+++ a2.py
@@ -11,6 +11,43 @@ __author__ = "Bradley Kent 45355194"
 
 # Write your classes here
 
+
+class Player:
+    pass
+
+
+class HumanPlayer(Player):
+    pass
+
+
+class ComputerPlayer(Player):
+    pass
+
+
+class Deck:
+    pass
+
+
+class Card:
+    pass
+
+
+class SkipCard(Card):
+    pass
+
+
+class ReverseCard(Card):
+    pass
+
+
+class Pickup2Card(Card):
+    pass
+
+
+class Pickup4Card(Card):
+    pass
+
+
 def main():
     print("Please run gui.py instead")
 
